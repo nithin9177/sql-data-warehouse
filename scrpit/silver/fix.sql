2f5f664e9213254daa5304b717f312741051e693
nithin9177 <78631594+nithin9177@users.noreply.github.com>
2026-02-06 12:22:43 +0530

    Create fix.sql

---
+++ scrpit/silver/fix.sql
@@ -0,0 +1,192 @@
+
+insert into silver.crm_cust_info(
+cst_id,
+cst_key,
+cst_firstname,
+cst_lastname,
+cst_marital_status,
+cst_gndr,
+cst_create_date)
+select 
+cst_id,
+cst_key,
+trim(cst_firstname) as cst_firstname,
+trim(cst_lastname) as cst_lastname,
+case
+ when UPPER(trim(cst_marital_status)) = 'S' THEN 'Single'
+ when UPPER(trim(cst_marital_status)) = 'M' THEN 'Married'
+ else 'n/a'
+end as cst_marital_status,
+case
+ when UPPER(trim(cst_gndr)) = 'F' THEN 'Female'
+ when UPPER(trim(cst_gndr)) = 'M' THEN 'Male'
+ else 'n/a'
+end as cst_gndr,
+cst_create_date
+from 
+(
+select *,
+ROW_NUMBER() over(partition by cst_id 
+order by cst_create_date desc
+) as cust_rank_by_date
+from bronze.crm_cust_info
+) as t
+where (cust_rank_by_date = 1 and cst_id is not null)   ;
+
+
+
+
+-- inserting the data in silver.crm_prd_info
+
+
+
+insert into silver.crm_prd_info(
+   prd_id ,
+   cat_id,
+    prd_key,
+    prd_nm,
+    prd_cost,
+    prd_line,
+    prd_start_dt,
+    prd_end_dt
+)
+SELECT
+prd_id,
+replace(substring(prd_key,1,5),'-','_') as cat_id,
+substring(prd_key,7, len(prd_key)) as prd_key,
+prd_nm,
+ISNULL(prd_cost,0) as prd_cost,
+case 
+ when upper(trim(prd_line)) = 'M' then 'Mountain'
+ when upper(trim(prd_line)) = 'S' then 'Other Sales'
+ when upper(trim(prd_line)) = 'T' then 'Touring'
+ when upper(trim(prd_line)) = 'R' then 'Road' 
+else 'n/a' 
+end as prd_line,
+CAST(prd_start_dt as date) as prd_start_dt,
+cast(lead(prd_start_dt) over(partition by prd_key order by prd_start_dt) - 1 as date) as prd_end_dt
+from
+bronze.crm_prd_info;
+
+
+
+
+
+
+
+
+
+insert  into silver.crm_sales_details(
+ sls_ord_num,
+    sls_prd_key,
+    sls_cust_id,
+    sls_order_dt,
+    sls_ship_dt,
+    sls_due_dt,
+    sls_sales,
+    sls_quantity,
+    sls_price
+)
+select 
+sls_ord_num,
+sls_prd_key,
+sls_cust_id,
+case
+ when sls_order_dt <= 0 or len(sls_order_dt) != 8 then null
+ else cast(cast(sls_order_dt as varchar) as date)
+end as sls_order_dt,
+case
+ when sls_ship_dt <= 0 or len(sls_ship_dt) != 8 then null
+ else cast(cast(sls_ship_dt as varchar) as date)
+end as sls_ship_dt,
+case 
+ when sls_due_dt <= 0 or len(sls_due_dt) != 8 then null
+ else cast(cast(sls_due_dt as varchar) as date)
+end as sls_due_dt,
+case
+ when sls_sales is null or sls_sales <= 0 or sls_sales != sls_quantity * ABS(sls_price)
+  then sls_quantity * abs(sls_price)
+  else sls_sales
+end as sls_sales,
+case
+ when sls_quantity is null or sls_quantity <= 0 or sls_quantity != sls_sales / ABS(sls_price)
+  then sls_sales / ABS(sls_price)
+  else sls_quantity
+end as sls_quantity,
+case
+ when sls_price is null or sls_price <= 0 or sls_price != sls_sales / nullif(sls_quantity,1)
+  then sls_sales / nullif(sls_quantity,1)
+  else sls_price
+end as sls_price
+from bronze.crm_sales_details 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+insert into silver.erp_cust_az12(
+cid,
+bdate,
+gen)
+SELECT
+case 
+ when cid like 'NAS%' then SUBSTRING(cid,4,len(cid)) 
+ else cid
+ end as cid_,
+case
+ when bdate > getdate() then NULL
+ else bdate
+ end as bdate,
+case 
+ when UPPER(TRIM(GEN)) in ('M','MALE') then 'Male'
+ when upper(trim(GEN)) in ('F','FEMALE') then 'Female'
+ else 'n/a'
+end as gen
+FROM bronze.erp_cust_az12
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+insert into silver.erp_loc_a101(
+cid,
+cntry
+)
+SELECT 
+REPLACE(cid,'-','') as cid,
+case 
+ when trim(cntry) = 'DE' then 'Germany'
+ when trim(cntry) in ('US','USA') then 'United States'
+ when trim(cntry) = '' or cntry is null then 'n/a'
+ else trim(cntry)
+end as cntry
+FROM bronze.erp_loc_a101;
